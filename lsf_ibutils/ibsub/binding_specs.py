990854d16abcf5f6b4708bd5cfa9c5d02fa156cf
Sean Fisk <sean@seanfisk.com>
2013-08-12 12:07:37 +0200

    Add script building function.

--- lsf_ibutils/ibsub/binding_specs.py
+++ lsf_ibutils/ibsub/binding_specs.py
@@ -1,11 +1,14 @@
 """:mod:`lsf_ibutils.ibsub.binding_specs` -- pinject setup
 """
 
+from datetime import datetime
+
+from pinject import BindingSpec
 
 from lsf_ibutils.ibsub.prompts import exec_prompts
-from pinject import BindingSpec
 
 
 class IbsubBindingSpec(BindingSpec):
     def configure(self, bind):
         bind('exec_prompts', to_instance=exec_prompts)
+        bind('today_datetime', to_class=datetime.today)
