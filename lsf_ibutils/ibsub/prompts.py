1a49b540064a7b716089c68e8dba4da4d9866918
Sean Fisk <sean@seanfisk.com>
2013-08-13 17:07:00 +0200

    Add more prompts.
    Some functions are currently untested, but tests will be added soon.

--- lsf_ibutils/ibsub/prompts.py
+++ lsf_ibutils/ibsub/prompts.py
@@ -2,8 +2,20 @@
 """
 
 from __future__ import print_function
+import re
+import os
+import subprocess
 
 import pinject
+
+# The two options for specifying flags are declarative and imperative. After
+# trying the declarative approach initially, we made the decision that no
+# amount of custom options for the declarative approach would allow it to be as
+# customized as some of the arguments needed. Instead, we opted for the
+# imperative approach, with each function built up from smaller, private
+# library functions.
+
+WALL_CLOCK_RE = re.compile(r'(?:\d\d?:)?\d\d?$')
 
 
 class Prompt(object):
@@ -30,6 +42,132 @@ class JobName(Prompt):
         return (text, ['-J', text])
 
 
+class ProjectCode(Prompt):
+    """Prompt for the project code, usually the same as the user's group."""
+    def __call__(self, values):
+        text = self._simple_prompt('Project code')
+        return (text, ['-P', text])
+
+
+class TasksPerNode(Prompt):
+    """Prompt for the number of MPI tasks to run on each node."""
+    def _validator(self, text):
+        # XXX TODO Untested
+        try:
+            n = int(text)
+        except ValueError:
+            return False
+        return n > 0
+
+    def __call__(self, values):
+        text = self._simple_prompt(
+            'Tasks per node',
+            format_='positive number',
+            validator=self._validator)
+        return (text, ['-R', 'span[ptile={0}]'.format(text)])
+
+
+class WallClockTime(Prompt):
+    """Prompt for the wall clock time limit of the task, in seconds."""
+    def _validator(self, text):
+        # XXX TODO Untested
+        return bool(WALL_CLOCK_RE.match(text))
+
+    def __call__(self, values):
+        text = self._simple_prompt(
+            'Wall clock time limit',
+            format_='00:00 for hours or 00 for minutes',
+            required=True,
+            validator=self._validator)
+        return (text, ['-W', text])
+
+
+class QueueName(Prompt):
+    """Prompt for the job queue name."""
+    @pinject.copy_args_to_internal_fields
+    def __init__(self, simple_prompt):
+        self._completions_cache = None
+
+    def _validator(self, text):
+        # XXX TODO Untested
+        return text in self._get_completions()
+
+    def _get_completions(self):
+        # XXX TODO Untested
+
+        # Retrieve memoized completions, if they exist.
+        if self._completions_cache is not None:
+            return self._completions_cache
+
+        bqueues_args = ['bqueues']
+        current_user = os.getenv('USER')
+        if current_user is not None:
+            # These arguments will get allowed queues for the current user.
+            bqueues_args += ['-u', current_user]
+        queues = []
+        try:
+            output = subprocess.check_output(bqueues_args)
+            # Strip off the first line, which is a header.
+            lines = output.splitlines()[1:]
+            for line in lines:
+                queue = line.split()[0]
+                queues.append(queue)
+        except subprocess.CalledProcessError:
+            pass
+        except OSError:
+            pass
+        # If the bqueues subprocess bombs out on an error, just ignore it.  The
+        # program can be useful even without completions. If we don't have
+        # bqueues our program should not crash.
+
+        # Memoize the completions. We also memoize an empty list of completions
+        # because if it didn't work a second ago, it's not likely to work now.
+        self._completions_cache = queues
+
+        return queues
+
+    def __call__(self, args_thus_far):
+        # XXX TODO Untested
+
+        # This is hard-coded to our particular machine, but it seems like a
+        # reasonable default everywhere.
+        default = 'regular'
+        text = self._simple_prompt(
+            'Queue',
+            required=True,
+            validator=self._validator,
+            default=default,
+            completions=self._get_completions(),
+        )
+        return ['-q', text]
+
+
+class OutputFileName(Prompt):
+    def __call__(self, values):
+        try:
+            default = '{0}.%J.out'.format(values[JobName])
+        except KeyError:
+            default = None
+        text = self._simple_prompt(
+            'Output file name',
+            default=default
+        )
+        return (text, ['-o', text])
+
+
+class ErrorFileName(Prompt):
+    def __call__(self, values):
+        try:
+            default = '{0}.%J.err'.format(values[JobName])
+        except KeyError:
+            default = None
+        text = self._simple_prompt(
+            'Error file name',
+            default=default
+        )
+        return (text, ['-e', text])
+
+
 class ExecPrompts(object):
     @pinject.copy_args_to_internal_fields
     def __init__(self, prompt_class_list, simple_prompt):
@@ -41,6 +179,7 @@ class ExecPrompts(object):
         :return: nested list of flags for each prompt
         :rtype: :class:`list` of :class:`list` of :class:`str`
         """
+        # XXX TODO Untested
         values = {}
         flags_list = []
         for prompt_class in self._prompt_class_list:
--- tests/ibsub/test_prompts.py
+++ tests/ibsub/test_prompts.py
@@ -1,7 +1,16 @@
-from mock import MagicMock
+from mock import MagicMock, sentinel
 from pytest import fixture
+import pytest
+parametrize = pytest.mark.paraemtrize
 
-from lsf_ibutils.ibsub.prompts import JobName
+from lsf_ibutils.ibsub.prompts import (
+    JobName,
+    ProjectCode,
+    TasksPerNode,
+    WallClockTime,
+)
+# For {Output,Error}FileName
+from lsf_ibutils.ibsub import prompts
 
 
 @fixture
@@ -15,9 +24,90 @@ class TestJobName(object):
         return JobName(mock_simple_prompt)
 
     def test_return_value(self, job_name, mock_simple_prompt):
-        mock_simple_prompt.return_value = 'awesome'
-        assert ('awesome', ['-J', 'awesome']) == job_name({})
+        mock_simple_prompt.return_value = sentinel.name
+        assert (sentinel.name, ['-J', sentinel.name]) == job_name({})
 
     def test_calls_simple_prompt_correctly(self, job_name, mock_simple_prompt):
         job_name({})
         mock_simple_prompt.assert_called_once_with('Job name', required=True)
+
+
+class TestProjectCode(object):
+    @fixture
+    def project_code(self, mock_simple_prompt):
+        return ProjectCode(mock_simple_prompt)
+
+    def test_return_value(self, project_code, mock_simple_prompt):
+        mock_simple_prompt.return_value = sentinel.code
+        assert (sentinel.code, ['-P', sentinel.code]) == project_code({})
+
+    def test_calls_simple_prompt_correctly(
+            self, project_code, mock_simple_prompt):
+        project_code({})
+        mock_simple_prompt.assert_called_once_with('Project code')
+
+
+class TestTasksPerNode(object):
+    @fixture
+    def tasks_per_node(self, mock_simple_prompt):
+        return TasksPerNode(mock_simple_prompt)
+
+    def test_return_value(self, tasks_per_node, mock_simple_prompt):
+        mock_simple_prompt.return_value = '230'
+        assert ('230', ['-R', 'span[ptile=230]']) == tasks_per_node({})
+
+    def test_calls_simple_prompt_correctly(
+            self, tasks_per_node, mock_simple_prompt):
+        tasks_per_node({})
+        mock_simple_prompt.assert_called_once_with(
+            'Tasks per node',
+            format_='positive number',
+            validator=tasks_per_node._validator)
+
+
+class TestWallClockTime(object):
+    @fixture
+    def wall_clock_time(self, mock_simple_prompt):
+        return WallClockTime(mock_simple_prompt)
+
+    def test_return_value(self, wall_clock_time, mock_simple_prompt):
+        mock_simple_prompt.return_value = sentinel.time
+        assert (sentinel.time, ['-W', sentinel.time]) == wall_clock_time({})
+
+    def test_calls_simple_prompt_correctly(
+            self, wall_clock_time, mock_simple_prompt):
+        wall_clock_time({})
+        mock_simple_prompt.assert_called_once_with(
+            'Wall clock time limit',
+            format_='00:00 for hours or 00 for minutes',
+            required=True,
+            validator=wall_clock_time._validator)
+
+
+class TestOutputErrorFileName(object):
+    @fixture(params=['Output', 'Error'])
+    def outerr(self, request):
+        return request.param
+
+    @fixture
+    def file_name(self, mock_simple_prompt, outerr):
+        return getattr(prompts, outerr + 'FileName')(mock_simple_prompt)
+
+    def test_return_value(self, outerr, file_name, mock_simple_prompt):
+        mock_simple_prompt.return_value = sentinel.file_name
+        assert (sentinel.file_name, [
+            '-' + outerr[0].lower(), sentinel.file_name]) == file_name({})
+
+    def test_calls_simple_prompt_correctly_without_job_name(
+            self, outerr, file_name, mock_simple_prompt):
+        file_name({})
+        mock_simple_prompt.assert_called_once_with(
+            outerr + ' file name',
+            default=None)
+
+    def test_calls_simple_prompt_correctly_with_job_name(
+            self, outerr, file_name, mock_simple_prompt):
+        file_name({JobName: 'my name'})
+        mock_simple_prompt.assert_called_once_with(
+            outerr + ' file name',
+            default='my name.%J.' + outerr[:3].lower())
