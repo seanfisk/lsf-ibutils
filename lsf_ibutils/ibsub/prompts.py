98095e85855d19ba48988dbca2240c9facd0e765
Sean Fisk <sean@seanfisk.com>
2013-10-11 01:34:27 -0600

    Fix issues in queue name completion code.
    - Read the output line-by-line instead of reading into a big string.
    - Handle errors in line format more smoothly.

--- lsf_ibutils/ibsub/prompts.py
+++ lsf_ibutils/ibsub/prompts.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 import os
 import subprocess
+from tempfile import TemporaryFile
 
 import pinject
 
@@ -234,15 +235,29 @@ class GetQueues(object):
             bqueues_args += ['-u', current_user]
         queues = []
         try:
-            output = subprocess.check_output(bqueues_args)
-            # Strip off the first line, which is a header.
-            lines = output.splitlines()[1:]
-            for line in lines:
-                queue = line.split()[0]
-                queues.append(queue)
+            # Process the file line-by-line instead of reading the whole file
+            # into memory. Should be more memory-efficient.
+            with TemporaryFile() as output_file:
+                subprocess.check_call(bqueues_args, stdout=output_file)
+                # NOTE: If the subprocess call bombs, the file still gets
+                # closed. Yay Python for doing the right thing!
+
+                # Jump to beginning of file.
+                output_file.seek(0)
+                # Strip off the first line, which is a header. If this doesn't
+                # exist, it will raise a StopIteration.
+                next(output_file)
+                # Grab queue names.
+                for line in output_file:
+                    tokens = line.split()
+                    if len(tokens) > 0:
+                        queues.append(tokens[0])
         except subprocess.CalledProcessError:
             pass
         except OSError:
+            pass
+        except StopIteration:
+            # Raised by the call to next() if there are no lines in the file.
             pass
         # If the bqueues subprocess bombs out on an error, just ignore it.  The
         # program can be useful even without completions. If we don't have
